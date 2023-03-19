204dc8847a7e108dae8dd9d359d241d40073d193
YuJeong <dbwjd7265@gmail.com>
2023-03-19 19:29:59 +0900

    제발 되라

--- 02_alg.js
+++ 02_alg.js
@@ -3,12 +3,12 @@
  * 받습니다.
 그리고 코더랜드 렌터카의 기본요금, 기본 시간, 기본 시간 이후에 시간당 부과되는 요금을 defaultPrice,
  * defaultHour, defaultCost 등 매개변수로 받습니다.
-두 업체의 정보를 토대로 비교하여 최소 비용을 return 하세요.
+두 업체의 정보를 토대로 비교하여 최소 비용을 return하세요.
  */
 
 function solution(hour, price, defaultPrice, defaultHour, defaultCost) {
-    const H = hour; //엘리스 토끼가 이용할 시간
-    const A = price; //모자장수 렌터카의 시간당 비용
+    const H = hour; //엘리스 토끼 이용 시간
+    const A = price; //모자장수 렌터카 시간당 비용
     const B = defaultPrice; //코더랜드 렌터카 기본요금
     const C = defaultHour; //코더랜드 렌터카 기본시간
     const D = defaultCost; //코더랜드 렌터카 기본시간 이후 시간당 부과 요금
